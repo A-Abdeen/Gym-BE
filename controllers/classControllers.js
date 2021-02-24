b2aed1b9edd1f6eba51c42516c02dafd38d31635
Mahmood28 <mahmoodalwatani@gmail.com>
2021-02-24 16:38:00 +0300

    classBook in controller.

--- controllers/classControllers.js
+++ controllers/classControllers.js
@@ -17,3 +17,21 @@ exports.classList = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.classBook = async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.user.id, {
+      include: {
+        model: Class,
+        as: "classes",
+      },
+    });
+    await user.addClass(req.class.id);
+    res.status(201).json({
+      ...user,
+      classes: [...user.classes, req.body.classId],
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/classes.js
+++ routes/classes.js
@@ -1,8 +1,23 @@
 const express = require("express");
 
-const { classList } = require("../controllers/classControllers");
+const { classList, classBook } = require("../controllers/classControllers");
 const router = express.Router();
 
+router.param("classId", async (req, res, next, classId) => {
+  const foundClass = await fetchGym(classId, next);
+  if (foundClass) {
+    req.class = foundClass;
+    next();
+  } else {
+    next({
+      status: 404,
+      message: "Class Not Found",
+    });
+  }
+});
+
 router.get("/", classList);
+
+router.put("/:classId/book", classBook);
 
 module.exports = router;
